d08f531697172fe19c5a1eb6e47dae00b9f8e73b
ebruchez <ebruchez@orbeon.com>
2018-02-07 10:52:24 -0800

    Stubs to allow building on MacOS

--- src/org/apache/html/dom/HTMLFrameElementImpl.java
+++ src/org/apache/html/dom/HTMLFrameElementImpl.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,6 +16,7 @@
  */
 package org.apache.html.dom;
 
+import org.w3c.dom.Document;
 import org.w3c.dom.html.HTMLFrameElement;
 
 /**
@@ -30,114 +31,119 @@ public class HTMLFrameElementImpl
     implements HTMLFrameElement
 {
 
-    private static final long serialVersionUID = 635237057173695984L; 
+    // ORBEON, to build on MacOS
+    public Document getContentDocument() {
+        return null;
+    }
+
+    private static final long serialVersionUID = 635237057173695984L;
 
     public String getFrameBorder()
     {
         return getAttribute( "frameborder" );
     }
-    
-    
+
+
     public void setFrameBorder( String frameBorder )
     {
         setAttribute( "frameborder", frameBorder );
     }
-  
-  
+
+
     public String getLongDesc()
     {
         return getAttribute( "longdesc" );
     }
-    
-    
+
+
     public void setLongDesc( String longDesc )
     {
         setAttribute( "longdesc", longDesc );
     }
-  
-  
+
+
     public String getMarginHeight()
     {
         return getAttribute( "marginheight" );
     }
-    
-    
+
+
     public void setMarginHeight( String marginHeight )
     {
         setAttribute( "marginheight", marginHeight );
     }
-  
-  
+
+
     public String getMarginWidth()
     {
         return getAttribute( "marginwidth" );
     }
-    
-    
+
+
     public void setMarginWidth( String marginWidth )
     {
         setAttribute( "marginwidth", marginWidth );
     }
-  
-  
+
+
     public String getName()
     {
         return getAttribute( "name" );
     }
-    
-    
+
+
     public void setName( String name )
     {
         setAttribute( "name", name );
     }
 
-    
+
     public boolean getNoResize()
     {
         return getBinary( "noresize" );
     }
-    
-    
+
+
     public void setNoResize( boolean noResize )
     {
         setAttribute( "noresize", noResize );
     }
 
-    
+
     public String getScrolling()
     {
         return capitalize( getAttribute( "scrolling" ) );
     }
-    
-    
+
+
     public void setScrolling( String scrolling )
     {
         setAttribute( "scrolling", scrolling );
     }
-  
-  
+
+
     public String getSrc()
     {
         return getAttribute( "src" );
     }
-    
-    
+
+
     public void setSrc( String src )
     {
         setAttribute( "src", src );
     }
 
-    
+
     /**
      * Constructor requires owner document.
-     * 
+     *
      * @param owner The owner HTML document
      */
     public HTMLFrameElementImpl( HTMLDocumentImpl owner, String name )
     {
         super( owner, name );
     }
-  
+
 
 }
 
--- src/org/apache/html/dom/HTMLIFrameElementImpl.java
+++ src/org/apache/html/dom/HTMLIFrameElementImpl.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,6 +16,7 @@
  */
 package org.apache.html.dom;
 
+import org.w3c.dom.Document;
 import org.w3c.dom.html.HTMLIFrameElement;
 
 /**
@@ -30,109 +31,114 @@ public class HTMLIFrameElementImpl
     implements HTMLIFrameElement
 {
 
+    // ORBEON, to build on MacOS
+    public Document getContentDocument() {
+        return null;
+    }
+
     private static final long serialVersionUID = 2393622754706230429L;
 
     public String getAlign()
     {
         return capitalize( getAttribute( "align" ) );
     }
-    
-    
+
+
     public void setAlign( String align )
     {
         setAttribute( "align", align );
     }
-  
-    
+
+
     public String getFrameBorder()
     {
         return getAttribute( "frameborder" );
     }
-    
-    
+
+
     public void setFrameBorder( String frameBorder )
     {
         setAttribute( "frameborder", frameBorder );
     }
 
-    
+
     public String getHeight()
     {
         return getAttribute( "height" );
     }
-    
-    
+
+
     public void setHeight( String height )
     {
         setAttribute( "height", height );
     }
-  
+
     public String getLongDesc()
     {
         return getAttribute( "longdesc" );
     }
-    
-    
+
+
     public void setLongDesc( String longDesc )
     {
         setAttribute( "longdesc", longDesc );
     }
-  
-  
+
+
     public String getMarginHeight()
     {
         return getAttribute( "marginheight" );
     }
-    
-    
+
+
     public void setMarginHeight( String marginHeight )
     {
         setAttribute( "marginheight", marginHeight );
     }
-  
-  
+
+
     public String getMarginWidth()
     {
         return getAttribute( "marginwidth" );
     }
-    
-    
+
+
     public void setMarginWidth( String marginWidth )
     {
         setAttribute( "marginwidth", marginWidth );
     }
 
-  
+
       public String getName()
     {
         return getAttribute( "name" );
     }
-    
-    
+
+
     public void setName( String name )
     {
         setAttribute( "name", name );
     }
 
-    
+
     public String getScrolling()
     {
         return capitalize( getAttribute( "scrolling" ) );
     }
-    
-    
+
+
     public void setScrolling( String scrolling )
     {
         setAttribute( "scrolling", scrolling );
     }
-    
-  
+
+
        public String getSrc()
     {
         return getAttribute( "src" );
     }
-    
-    
+
+
     public void setSrc( String src )
     {
         setAttribute( "src", src );
@@ -143,17 +149,17 @@ public class HTMLIFrameElementImpl
     {
         return getAttribute( "width" );
     }
-    
-    
+
+
     public void setWidth( String width )
     {
         setAttribute( "width", width );
     }
 
-    
+
     /**
      * Constructor requires owner document.
-     * 
+     *
      * @param owner The owner HTML document
      */
     public HTMLIFrameElementImpl( HTMLDocumentImpl owner, String name )
